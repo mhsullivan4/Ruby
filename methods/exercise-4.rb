54c3c20a9c6f57a357e57faddd52f8ad5e1c3ceb
Marie Sullivan <mhsullivan@proton.me>
2022-10-12 15:17:55 -0700

    update

--- methods/exercise-4.rb
+++ methods/exercise-4.rb
@@ -12,13 +12,3 @@ scream("Yippeee")
 # prints nothing as you return and ruby exits immediately so line 7 never executed
 
 
-# fix
-
-1. def scream(words)
-      words = words + "!!!!"
-      return words
-      puts words
-    end
-
-  p scream("Yippeee")
-
--- methods/exercise-6.rb
+++ methods/exercise-6.rb
@@ -13,11 +13,3 @@ end
 calculate_product(4)        # one arguement
 
 
-def calculate_product(num1,num2)
-  num1 * num2
-end
-calculate_product(4,2)
-
-print calculate_product
-
-# runs without error but have to put p or puts to display answer
